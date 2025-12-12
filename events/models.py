02c41a697953c713a8c13b85a02c0a0917a8248b
orgho Das <mcguireon@gmail.com>
2025-12-12 14:46:12 +0600

    event app commit

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    scheduled_for = models.DateTimeField()
+    max_seats = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class EventRegistration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    registered_on = models.DateTimeField(auto_now_add=True)
