cb533485d9f78aed61314f5673d6fa6978cf2c3d
orgho Das <mcguireon@gmail.com>
2025-12-12 14:57:37 +0600

    payment app commit

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Transaction(models.Model):
+    PAYMENT_TYPES = [
+        ('course', 'Course Enrollment'),
+        ('event', 'Event Registration'),
+        ('payout', 'Instructor Payout')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_type = models.CharField(max_length=20, choices=PAYMENT_TYPES)
+    status = models.CharField(max_length=20, default='pending')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
